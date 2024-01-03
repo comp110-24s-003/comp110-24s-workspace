a939bf8d727f03a19e95e46a0ae0cc884ee851c1
Kris Jordan <kris@cs.unc.edu>
2024-01-03 08:51:19 -0500

    welcome.py added to greet new students

---
+++ welcome.py
@@ -0,0 +1,7 @@
+"""A welcoming test program to start COMP110"""
+
+__author__ = "Kris Jordan"
+
+print("Welcome to the Spring 2024 of COMP110-003!")
+print("You are in for a fun adventure into the woods of programming.")
+print("<3 Kris & the COMP110 UTA Team")
